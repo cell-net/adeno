321432093ca6137ffb46ba21d3eba5e292607fea
Rivaan <ran.rana@kcl.ac.uk>
2023-08-25 00:18:36 +0100

    Updating Adeno to add permit and address audit findings

--- src/Adeno.sol
+++ src/Adeno.sol
@@ -1,16 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "openzeppelin/token/ERC20/ERC20.sol";
+import "openzeppelin/token/ERC20/extensions/ERC20Permit.sol";
 import "openzeppelin/token/ERC20/extensions/ERC20Burnable.sol";
 import "openzeppelin/security/Pausable.sol";
 import "openzeppelin/access/Ownable.sol";
 
-contract Adeno is ERC20, ERC20Burnable, Pausable, Ownable {
+contract Adeno is ERC20Permit, ERC20Burnable, Pausable, Ownable {
     uint256 public maxSupply;
     mapping(address => bool) public whitelist;
+    bytes32 private constant _PERMIT_TYPEHASH =
+            keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
 
-    constructor(uint256 _maxSupply) ERC20("Adeno", "ADE") {
+    constructor(uint256 _maxSupply) ERC20("Adeno", "ADE") ERC20Permit("Adeno") Ownable(msg.sender) {
         whitelist[_msgSender()] = true;
         maxSupply = _maxSupply;
     }
@@ -23,32 +25,58 @@ contract Adeno is ERC20, ERC20Burnable, Pausable, Ownable {
         _unpause();
     }
 
-    function transfer(address to, uint256 amount) public virtual override whenNotPaused returns (bool) {
+    function transfer(address to, uint256 amount) public override whenNotPaused returns (bool) {
+        require(to != address(0));
         address owner = _msgSender();
         _transfer(owner, to, amount);
         return true;
     }
 
-    function approve(address spender, uint256 amount) public virtual override whenNotPaused returns (bool) {
+    function approve(address spender, uint256 amount) public override whenNotPaused returns (bool) {
         address owner = _msgSender();
         _approve(owner, spender, amount);
         return true;
     }
 
-    function transferFrom(address from, address to, uint256 amount) public virtual override whenNotPaused returns (bool) {
+    function permit(
+        address owner,
+        address spender,
+        uint256 value,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) public override whenNotPaused {
+        if (block.timestamp > deadline) {
+            revert ERC2612ExpiredSignature(deadline);
+        }
+
+        bytes32 structHash = keccak256(abi.encode(_PERMIT_TYPEHASH, owner, spender, value, _useNonce(owner), deadline));
+
+        bytes32 hash = _hashTypedDataV4(structHash);
+
+        address signer = ECDSA.recover(hash, v, r, s);
+        if (signer != owner) {
+            revert ERC2612InvalidSigner(signer, owner);
+        }
+
+        _approve(owner, spender, value);
+    }
+
+    function transferFrom(address from, address to, uint256 amount) public override whenNotPaused returns (bool) {
         address spender = _msgSender();
         _spendAllowance(from, spender, amount);
         _transfer(from, to, amount);
         return true;
     }
 
-    function increaseAllowance(address spender, uint256 addedValue) public virtual override whenNotPaused returns (bool) {
+    function increaseAllowance(address spender, uint256 addedValue) public override whenNotPaused returns (bool) {
         address owner = _msgSender();
         _approve(owner, spender, allowance(owner, spender) + addedValue);
         return true;
     }
 
-    function decreaseAllowance(address spender, uint256 subtractedValue) public virtual override whenNotPaused returns (bool) {
+    function decreaseAllowance(address spender, uint256 subtractedValue) public override whenNotPaused returns (bool) {
         address owner = _msgSender();
         uint256 currentAllowance = allowance(owner, spender);
         require(currentAllowance >= subtractedValue, "ERC20: decreased allowance below zero");
@@ -64,7 +92,7 @@ contract Adeno is ERC20, ERC20Burnable, Pausable, Ownable {
         _mint(to, amount);
     }
 
-    function burn(uint256 amount) public virtual override whenNotPaused {
+    function burn(uint256 amount) public override whenNotPaused {
         _burn(_msgSender(), amount);
     }
 
--- test/Adeno.t.sol
+++ test/Adeno.t.sol
@@ -11,6 +11,8 @@ contract AdenoTest is Test {
     address otherContract1 = vm.addr(0x2);
     address otherContract2 = vm.addr(0x3);
     address user = vm.addr(0x4);
+    address _buyer = vm.addr(0x5);
+    address _buyer2 = vm.addr(0x6);
 
     uint256 private MAX_SUPPLY = 2625000000e18;
     uint256 private TREASURY_BAL = 10e18;
@@ -42,6 +44,43 @@ contract AdenoTest is Test {
     function testMint() public {
         adenoToken.mint(treasury, TREASURY_BAL);
         assertEq(adenoToken.balanceOf(treasury), TREASURY_BAL);
+    }
+
+    function testAdenoPermitAndTransferFrom() public {
+        adenoToken.mint(_buyer, 100e18);
+        uint256 amount = 100e18;
+
+        bytes32 adenoDomainSeparator = bytes32(adenoToken.DOMAIN_SEPARATOR());
+        bytes32 structHash = keccak256(abi.encode(0x6e71edae12b1b97f4d1f60370fef10105fa2faae0126114a169c64845d6126c9, _buyer, _buyer2, amount, adenoToken.nonces(_buyer) , block.timestamp + 3600));
+
+        bytes32 messageHash = keccak256(
+            abi.encodePacked(
+                "\x19\x01",
+                adenoDomainSeparator,
+                structHash
+            )
+        );
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(0x5, messageHash);
+
+        address signer = ecrecover(messageHash, v, r, s);
+        assertEq(signer, _buyer);
+
+        assertEq(adenoToken.balanceOf(_buyer), 100e18);
+        assertEq(adenoToken.balanceOf(_buyer2), 0);
+        assertEq(adenoToken.allowance(_buyer, _buyer2), 0);
+        vm.prank(_buyer);
+        adenoToken.permit(_buyer, _buyer2, 100e18, block.timestamp + 3600, v, r, s);
+        assertEq(adenoToken.allowance(_buyer, _buyer2), 100e18);
+        vm.prank(_buyer2);
+        adenoToken.transferFrom(_buyer, _buyer2, 98e18);
+        assertEq(adenoToken.allowance(_buyer, _buyer2), 2e18);
+        assertEq(adenoToken.balanceOf(_buyer), 2e18);
+        vm.prank(_buyer2);
+        adenoToken.transferFrom(_buyer, _buyer2, 1e18);
+        bytes4 selector = bytes4(keccak256("ERC20InsufficientAllowance(address,uint256,uint256)"));
+        vm.expectRevert(abi.encodeWithSelector(selector, address(0x7FA9385bE102ac3EAc297483Dd6233D62b3e1496), 0, 2e18));
+        adenoToken.transferFrom(_buyer, _buyer2, 2e18);
     }
 
     function testFailExceedsMintSupply() public {
@@ -113,14 +152,15 @@ contract AdenoTest is Test {
         adenoToken.approve(user2, 70e18);
         assertEq(adenoToken.allowance(user, user2), 70e18);
         adenoToken.pause();
-        vm.expectRevert("Pausable: paused");
+        bytes4 selector = bytes4(keccak256("EnforcedPause()"));
+        vm.expectRevert(selector);
         adenoToken.mint(user, 200e18);
-        vm.expectRevert("Pausable: paused");
+        vm.expectRevert(selector);
         adenoToken.transfer(user2, 10e18);
-        vm.expectRevert("Pausable: paused");
+        vm.expectRevert(selector);
         hoax(user);
         adenoToken.approve(user2, 100e18);
-        vm.expectRevert("Pausable: paused");
+        vm.expectRevert(selector);
         hoax(user2);
         adenoToken.transferFrom(user, treasury, 10e18);
 
